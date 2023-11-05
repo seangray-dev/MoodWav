ad32f2d680caff97ffb4f3a838113def71810f96
Sean Gray <seangray.dev@gmail.com>
2023-11-05 10:29:40 -0500

    setup Supabase + start UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,33 @@
-import { GeistSans } from 'geist/font'
-import './globals.css'
-
+import { Montserrat_Alternates } from '@next/font/google';
+import './globals.css';
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000'
+  : 'http://localhost:3000';
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
-}
+  title: 'MoodWav',
+  description:
+    'Providing personalized mood profiles based on recent Spotify listening history.',
+};
+
+const MA = Montserrat_Alternates({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
+    <html lang='en' className={MA.className}>
+      <body className='bg-background text-foreground'>
+        <main className='min-h-screen flex flex-col items-center w-full'>
           {children}
         </main>
       </body>
     </html>
-  )
+  );
 }
