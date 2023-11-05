79a3203b2e0f904b86b4efbbc7cc2a2ab809289d
Sean Gray <seangray.dev@gmail.com>
2023-11-05 17:02:10 -0500

    add useEffect to get session and redirect to /mood if logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import SpotifyButton from '@/components/SpotifyButton';
+'use client';
+
 import Footer from '@/components/layout/Footer';
 import Header from '@/components/layout/Header';
 import SpotifyMoodPrompt from '@/components/layout/SpotifyMoodPrompt';
+import { supabase } from '@/utils/supabase/client';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
-export default async function Index() {
+export default function Index() {
+  const router = useRouter();
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session }, error }) => {
+      console.log(session);
+      if (session) {
+        router.push('/mood');
+      }
+    });
+  }, [router]);
+
   return (
     <>
       <div className='flex-1 w-full flex flex-col items-center'>
