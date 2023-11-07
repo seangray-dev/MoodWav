f51919dd9d207f0de3a3a10adc52ad3f12bda8d7
Sean Gray <seangray.dev@gmail.com>
2023-11-06 19:51:48 -0500

    render different gradient bg depending on mood

--- app/mood/page.tsx
+++ app/mood/page.tsx
@@ -68,17 +68,45 @@ const Mood = () => {
     reflective: 'text-indigo-900',
   };
 
+  const moodGradient = {
+    joyful: 'bg-gradient-to-b from-yellow-400 to-yellow-300',
+    relaxed: 'bg-gradient-to-b from-green-400 to-green-300',
+    sad: 'bg-gradient-to-b from-blue-400 to-blue-300',
+    angry: 'bg-gradient-to-b from-red-500 to-red-300',
+    energetic: 'bg-gradient-to-b from-orange-400 to-orange-300',
+    reflective: 'bg-gradient-to-b from-indigo-400 to-indigo-300',
+  };
+
+  const moodGradientReverse = {
+    joyful: 'bg-gradient-to-t from-yellow-400 to-yellow-300',
+    relaxed: 'bg-gradient-to-t from-green-400 to-green-300',
+    sad: 'bg-gradient-to-t from-blue-400 to-blue-300',
+    angry: 'bg-gradient-to-t from-red-500 to-red-300',
+    energetic: 'bg-gradient-to-t from-orange-400 to-orange-300',
+    reflective: 'bg-gradient-to-t from-indigo-400 to-indigo-300',
+  };
+
   return (
     <div className='flex flex-1 flex-col place-content-center moodring w-full'>
       {moodData ? (
         <div className='container'>
           <Card className='border-none mx-auto flex flex-col gap-6 md:grid md:grid-cols-2 md:max-w-[736px] rounded-[32px]'>
-            <Card className='md:flex md:flex-col md:place-content-center md:gap-0 md:justify-evenly border-none bg-gradient-to-b from-indigo-500 to-indigo-700 rounded-[32px] flex flex-col gap-4 pb-10 py-6'>
+            <Card
+              className={`md:flex md:flex-col md:place-content-center md:gap-0 md:justify-evenly border-none rounded-[32px] flex flex-col gap-4 pb-10 py-6 ${
+                moodGradient[
+                  moodData.highestMood.toLowerCase() as keyof typeof moodGradient
+                ]
+              } `}>
               <p className='text-center font-medium text-lg text-muted md:text-2xl'>
                 Your Mood
               </p>
               <div className='flex justify-center'>
-                <div className='flex flex-col justify-center items-center rounded-full w-[140px] h-[140px] md:w-[200px] md:h-[200px] bg-gradient-to-b from-indigo-700 to-indigo-500'>
+                <div
+                  className={`flex flex-col justify-center items-center rounded-full w-[140px] h-[140px] md:w-[200px] md:h-[200px] ${
+                    moodGradientReverse[
+                      moodData.highestMood.toLowerCase() as keyof typeof moodGradient
+                    ]
+                  } `}>
                   <span className='text-[56px] md:text-[72px] text-white font-bold'>
                     {moodData.highestScore.toFixed(1)}
                   </span>
