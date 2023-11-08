332cd5c7e650c9125da63cc9e010790c26e5725b
Sean Gray <seangray.dev@gmail.com>
2023-11-08 11:41:51 -0500

    fix TS errors

--- app/mood/page.tsx
+++ app/mood/page.tsx
@@ -1,11 +1,7 @@
 'use client';
 
-import SpotifyLogo from '@/assets/images/Spotify_Icon_RGB_Green.png';
 import MoodWavLogo from '@/assets/images/moodwav-high-resolution-logo-transparent.png';
 import Footer from '@/components/layout/Footer';
-import { Button } from '@/components/ui/button';
-import { Card } from '@/components/ui/card';
-import { Skeleton } from '@/components/ui/skeleton';
 import { determineUserMood } from '@/utils/mood_calculations/calculations';
 import {
   TrackDetail,
@@ -13,12 +9,13 @@ import {
   fetchRecentlyPlayedTracks,
   fetchSpotifyUserID,
 } from '@/utils/spotify/spotify';
-import { ShareIcon } from 'lucide-react';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import ErrorAlert from './Error';
 import LoadingData from './LoadingData';
+import MoodScoreCard from './MoodScoreCard';
 import NoMoodData from './NoMoodData';
+import RecentlyPlayed from './RecentlyPlayed';
 
 const Mood = () => {
   const [spotifyUserID, setSpotifyUserID] = useState<string | undefined>(
@@ -32,7 +29,6 @@ const Mood = () => {
     allMoods: Record<string, number>;
   } | null>(null);
   const [recentTracks, setRecentTracks] = useState<TrackDetail[]>([]);
-  const [imageLoaded, setImageLoaded] = useState<Record<string, boolean>>({});
 
   useEffect(() => {
     const fetchProfileData = async () => {
@@ -68,46 +64,6 @@ const Mood = () => {
     fetchProfileData();
   }, []);
 
-  const moodBG = {
-    joyful: 'bg-yellow-200',
-    relaxed: 'bg-green-300',
-    sad: 'bg-blue-300',
-    angry: 'bg-red-300',
-    energetic: 'bg-orange-300',
-    reflective: 'bg-indigo-300',
-  };
-
-  const moodTextColor = {
-    joyful: 'text-yellow-900',
-    relaxed: 'text-green-900',
-    sad: 'text-blue-900',
-    angry: 'text-red-900',
-    energetic: 'text-orange-900',
-    reflective: 'text-indigo-900',
-  };
-
-  const moodGradient = {
-    joyful: 'bg-gradient-to-b from-yellow-400 to-yellow-300',
-    relaxed: 'bg-gradient-to-b from-green-400 to-green-300',
-    sad: 'bg-gradient-to-b from-blue-400 to-blue-300',
-    angry: 'bg-gradient-to-b from-red-500 to-red-300',
-    energetic: 'bg-gradient-to-b from-orange-400 to-orange-300',
-    reflective: 'bg-gradient-to-b from-indigo-400 to-indigo-300',
-  };
-
-  const moodGradientReverse = {
-    joyful: 'bg-gradient-to-t from-yellow-400 to-yellow-300',
-    relaxed: 'bg-gradient-to-t from-green-400 to-green-300',
-    sad: 'bg-gradient-to-t from-blue-400 to-blue-300',
-    angry: 'bg-gradient-to-t from-red-500 to-red-300',
-    energetic: 'bg-gradient-to-t from-orange-400 to-orange-300',
-    reflective: 'bg-gradient-to-t from-indigo-400 to-indigo-300',
-  };
-
-  const handleImageLoaded = (imageId: string) => {
-    setImageLoaded((prev) => ({ ...prev, [imageId]: true }));
-  };
-
   if (loading) {
     return <LoadingData />;
   }
@@ -130,87 +86,8 @@ const Mood = () => {
           width={300}
           height={200}
         />
-        <Card className='flex-1 border-none mx-auto flex flex-col gap-6 md:grid md:grid-cols-2 rounded-[32px] bg-primary drop-shadow-2xl'>
-          <Card
-            className={`md:flex md:flex-col md:place-content-center md:gap-0 md:justify-evenly  md:rounded-none md:rounded-l-[32px] flex flex-col gap-4 pb-10 py-6  rounded-t-[32px] rounded-b-none
-            bg-primary border-transparent md:border-r md:border-r-muted-foreground border-b-muted-foreground md:border-b-0 
-             `}>
-            <p className='text-center font-medium text-lg text-muted md:text-2xl'>
-              Your Mood
-            </p>
-            <div className='flex justify-center'>
-              <div
-                className={`flex flex-col justify-center items-center rounded-full w-[140px] h-[140px] md:w-[200px] md:h-[200px] ${
-                  moodGradientReverse[
-                    moodData.highestMood.toLowerCase() as keyof typeof moodGradient
-                  ]
-                } `}>
-                <span className='text-[56px] md:text-[72px] text-white font-bold'>
-                  {moodData.highestScore.toFixed(1)}
-                </span>
-              </div>
-            </div>
-            <p className='text-center text-white text-2xl font-bold md:text-[32px]'>
-              {moodData.highestMood.toLocaleUpperCase()}
-            </p>
-          </Card>
-          <div className='px-8 flex flex-col gap-4 pb-8 md:pb-[46px] md:gap-[28px]'>
-            <h2 className='text-white font-medium md:text-2xl md:mt-[38px]'>
-              Summary
-            </h2>
-            <ul className='flex flex-col gap-4'>
-              {Object.entries(moodData.allMoods).map(([mood, score]) => (
-                <li
-                  className={`flex justify-between text-sm px-4 py-[18px] rounded-lg ${
-                    moodBG[mood as keyof typeof moodBG]
-                  } ${moodTextColor[mood as keyof typeof moodTextColor]}`}
-                  key={mood}>
-                  <span className='font-medium'>
-                    {mood.toLocaleUpperCase()}
-                  </span>
-                  <span className='font-bold'>{score.toFixed(1)}%</span>
-                </li>
-              ))}
-            </ul>
-            <Button className='rounded-[288px] py-8 text-[18px] flex gap-3 items-center bg-primary-foreground text-card-foreground hover:text-white hover:bg-transparent border border-white transition-all duration-300'>
-              <span>Share</span>
-              <ShareIcon size={18} />
-            </Button>
-          </div>
-        </Card>
-
-        <div className='mt-20 mb-10 flex flex-col'>
-          <h2 className='text-lg md:text-2xl mb-6 font-medium'>
-            Recent Played
-          </h2>
-          <ul className='flex flex-col gap-4 md:grid md:grid-cols-2 lg:grid-cols-3'>
-            {recentTracks.map((track) => (
-              <li
-                className='hover:scale-[105%] transition-all duration-300 w-full'
-                key={track.id}>
-                <Card className='w-full flex items-center gap-6 bg-primary text-primary-foreground border-none drop-shadow-2xl'>
-                  {!imageLoaded[track.id] && (
-                    <Skeleton className='w-1/4 h-[100px]' />
-                  )}
-                  <img
-                    className={`w-1/4 ${
-                      imageLoaded[track.id] ? 'block' : 'hidden'
-                    }`}
-                    src={track.coverArt ? track.coverArt : SpotifyLogo.src}
-                    alt={track.name}
-                    onLoad={() => handleImageLoaded(track.id)}
-                  />
-                  <div className='flex flex-col gap-2 overflow-hidden'>
-                    <p className='font-bold truncate'>{track.name}</p>
-                    <p className='text-muted-foreground truncate'>
-                      {track.artistName}
-                    </p>
-                  </div>
-                </Card>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <MoodScoreCard moodData={moodData} />
+        <RecentlyPlayed recentTracks={recentTracks} />
       </div>
       <Footer />
     </div>
