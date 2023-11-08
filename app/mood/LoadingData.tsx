fbf7d8a9948e5be7f66e1b7393308543804d8cff
Sean Gray <seangray.dev@gmail.com>
2023-11-07 21:03:29 -0500

    change loading message

--- app/mood/LoadingData.tsx
+++ app/mood/LoadingData.tsx
@@ -5,7 +5,7 @@ const LoadingData = () => {
     <div className='moodring flex flex-1 w-full justify-center place-content-center flex-col'>
       <div className='mx-auto flex flex-col gap-4 font-medium'>
         <Loader2 className='h-10 w-10 animate-spin mx-auto' />
-        <p>Loading mood data...</p>
+        <p>Analyzing Your Mood...</p>
       </div>
     </div>
   );
