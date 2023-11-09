e4cc705e638a36fd6b6a7a7ef1567333d642bbc2
Sean Gray <seangray.dev@gmail.com>
2023-11-09 10:17:16 -0500

    add signout button at top of mood page

--- app/mood/page.tsx
+++ app/mood/page.tsx
@@ -2,6 +2,7 @@
 
 import MoodWavLogo from '@/assets/images/moodwav-high-resolution-logo-transparent.png';
 import Footer from '@/components/layout/Footer';
+import { Button } from '@/components/ui/button';
 import { determineUserMood } from '@/utils/mood_calculations/calculations';
 import {
   TrackDetail,
@@ -9,7 +10,9 @@ import {
   fetchRecentlyPlayedTracks,
   fetchSpotifyUserID,
 } from '@/utils/spotify/spotify';
+import { supabase } from '@/utils/supabase/client';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import ErrorAlert from './Error';
 import LoadingData from './LoadingData';
@@ -18,6 +21,8 @@ import NoMoodData from './NoMoodData';
 import RecentlyPlayed from './RecentlyPlayed';
 
 const Mood = () => {
+  // Local State
+  const router = useRouter();
   const [spotifyUserID, setSpotifyUserID] = useState<string | undefined>(
     undefined
   );
@@ -30,6 +35,8 @@ const Mood = () => {
   } | null>(null);
   const [recentTracks, setRecentTracks] = useState<TrackDetail[]>([]);
   const [selectedMood, setSelectedMood] = useState<string | null>(null);
+
+  // Functions
 
   useEffect(() => {
     const fetchProfileData = async () => {
@@ -65,6 +72,21 @@ const Mood = () => {
     fetchProfileData();
   }, []);
 
+  const signOut = async () => {
+    try {
+      const { error } = await supabase.auth.signOut();
+      if (error) throw error;
+
+      // Clearing the session storage or any other client-side storage where the token is saved
+      sessionStorage.clear();
+
+      // Redirect to login or home page after successful sign out
+      router.push('/');
+    } catch (error) {
+      console.error('Error during sign out:', error);
+    }
+  };
+
   if (loading) {
     return <LoadingData />;
   }
@@ -79,6 +101,16 @@ const Mood = () => {
 
   return (
     <div className='flex flex-1 flex-col place-content-center moodring w-full'>
+      <div className='flex justify-end container'>
+        <Button
+          onClick={async () => {
+            await signOut();
+          }}
+          variant={'ghost'}
+          className='flex-end hover:bg-transparent hover:text-white hover:underline transition-all duration-300 pt-6'>
+          Sign out
+        </Button>
+      </div>
       <div className='md:container px-2 mt-20'>
         <Image
           className='mb-10 mx-auto'
