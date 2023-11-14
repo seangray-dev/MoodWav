2167133b8efdf2dce641d12993b53ea61bee9bf3
Sean Gray <seangray.dev@gmail.com>
2023-11-14 18:02:00 -0500

    add bg to footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,33 +4,39 @@ import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <footer className='w-full border-t border-t-foreground/10 p-8 flex flex-col gap-10 justify-center text-xs md:flex-row md:justify-between md:container items-center'>
-      <div className='flex flex-col gap-4'>
-        {/* <p>
+    <footer className='w-full p-8 text-xs bg-secondary-foreground'>
+      <div className='flex flex-col items-center gap-10 md:container md:justify-between md:flex-row'>
+        <div className='flex flex-col gap-4'>
+          {/* <p>
           Built by{' '}
           <a
-            href='https://www.seangray.tech'
-            target='_blank'
-            className='font-bold hover:underline'
-            rel='noreferrer'>
-            Sean Gray
+          href='https://www.seangray.tech'
+          target='_blank'
+          className='font-bold hover:underline'
+          rel='noreferrer'>
+          Sean Gray
           </a>
         </p> */}
-        <a href='/'>
-          <Image
-            className='mx-auto'
-            src={MoodWavLogo}
-            alt='moodwav logo'
-            width={200}
-            height={200}
-          />
-        </a>
-      </div>
-      <div className='flex items-center gap-4 justify-center'>
-        <p>Powered by</p>
-        <a target='_blank' href='https://spotify.com'>
-          <img className='w-[100px]' src={SpotifyLogo.src} alt='spotify logo' />
-        </a>
+          <a href='/'>
+            <Image
+              className='mx-auto'
+              src={MoodWavLogo}
+              alt='moodwav logo'
+              width={200}
+              height={200}
+            />
+          </a>
+        </div>
+        <div className='flex items-center gap-4 justify-center'>
+          <p>Powered by</p>
+          <a target='_blank' href='https://spotify.com'>
+            <img
+              className='w-[100px]'
+              src={SpotifyLogo.src}
+              alt='spotify logo'
+            />
+          </a>
+        </div>
       </div>
     </footer>
   );
