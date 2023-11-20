958edfd0cc4c941aa380101149c80571ec513046
Sean Gray <seangray.dev@gmail.com>
2023-11-19 20:21:26 -0500

    add request access button

--- components/layout/SpotifyMoodPrompt.tsx
+++ components/layout/SpotifyMoodPrompt.tsx
@@ -1,16 +1,33 @@
+import { useRouter } from 'next/navigation';
 import SpotifyButton from '../SpotifyButton';
+import { Button } from '../ui/button';
 
 const SpotifyMoodPrompt = () => {
+  const router = useRouter();
+
+  const handleRequestAccess = () => {
+    router.push('/request-access');
+  };
+
   return (
     <>
       <h2 className='text-2xl md:text-4xl font-bold text-center my-4'>
         Discover Your Mood
       </h2>
-      <p className='text-center mb-6'>
+      {/* <p className='text-center mb-6'>
         Connect with Spotify to analyze your mood based on recently played
         songs.
-      </p>
-      <SpotifyButton />
+      </p> */}
+      <div className='text-center mb-6'>
+        Currently access to MoodWav is by invitation only due to Spotify API
+        restrictions.
+      </div>
+      <div className='flex justify-center gap-2'>
+        <SpotifyButton />
+        <Button onClick={handleRequestAccess} variant={'secondary'}>
+          Request Access
+        </Button>
+      </div>
     </>
   );
 };
