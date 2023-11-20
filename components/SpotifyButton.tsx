f92dbbfce97a9fa43b7ac2409307712f0e72fbe1
Sean Gray <seangray.dev@gmail.com>
2023-11-19 20:20:11 -0500

    remove mx-auto class

--- components/SpotifyButton.tsx
+++ components/SpotifyButton.tsx
@@ -26,7 +26,7 @@ const SpotifyButton = () => {
   return (
     <Button
       onClick={signInWithSpotify}
-      className='mx-auto flex gap-2 items-center hover:bg-white hover:text-black duration-300 transition-all'>
+      className='flex gap-2 items-center hover:bg-white hover:text-black duration-300 transition-all'>
       <Image alt='spotify logo' width={20} height={20} src={SpotifyLogo} />
       <div className='font-medium'>Login with Spotify</div>
     </Button>
