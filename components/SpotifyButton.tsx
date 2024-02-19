3e222ae0264cbf0670ceafb4de3004e9bf810a76
Sean Gray <seangray.dev@gmail.com>
2024-02-19 16:48:29 -0500

    Add user-top-read scope to SpotifyButton

--- components/SpotifyButton.tsx
+++ components/SpotifyButton.tsx
@@ -11,7 +11,7 @@ const SpotifyButton = () => {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'spotify',
         options: {
-          scopes: 'user-read-recently-played',
+          scopes: 'user-read-recently-played user-top-read',
         },
       });
 
